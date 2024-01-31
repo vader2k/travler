fcab0ab0950dc8323d8b72a5273240fbb47f4e0a
danie odukwe <kingdanny295@gmail.com>
2024-01-31 11:56:09 +0100

    fixed my name extension error

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
+import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
-
+import Button from "@/components/Button"
 const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
@@ -9,11 +10,27 @@ const Navbar = () => {
                 src="/hilink-logo.svg"
                 alt="logo" 
                 width={74} height={29} />
+        </Link>      
+        <ul className="hidden h-full gap-12 lg:flex"> 
+            {NAV_LINKS.map((link) => (
+                <Link 
+                    href={link.href} 
+                    key={link.key}
+                    className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+                >
+                    {link.label}
+                </Link>
+            ))}
+        </ul>
 
-            <ul className="hidden h-full gap-12 lg:flex"> 
-                
-            </ul>
-        </Link>
+        <div className="lg:flexCenter hidden">
+            <Button 
+                type ='button'
+                title = "Login"
+                icon = "/user.svg"
+                style= 'btn_dark_green' 
+            />
+        </div>
     </nav>
   )
 }
